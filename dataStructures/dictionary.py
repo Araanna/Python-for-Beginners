d21eec968c25fa3b2f8a6131f34ea4e10a07d188
Melanie Abalde <melanieabalde0@gmail.com>
2024-06-25 20:34:32 +0800

    Data Structures and Algorithms

---
+++ dataStructures/dictionary.py
@@ -0,0 +1,6 @@
+houses = {"Harry" : "Gryffindor", "Draco" : "Slytherine" }
+
+houses ["Hermione"] = "Gryffindor"
+
+print(houses [ "Harry" ])
+print(houses [ "Hermione" ])
---
+++ dataStructures/seqeunces.py
@@ -0,0 +1,21 @@
+#Sequences
+
+
+#List - sequence of mutable values
+name = "Harry"
+
+print (name [0])
+
+names = ["Harry", "Ron", "Hermione"]
+
+print(names)
+
+print (names [0])
+
+
+#Tuple - sequence of immutable values
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
